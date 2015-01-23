592a83d33f08cfee8bbfcc66997306bc1fcbf3ec
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-01-24 00:20:52 +0100

    RouleTaBoule

---
+++ New Unity Project 1/Assets/Script/PlayerControler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PlayerControler : MonoBehaviour {
+	public float speed;
+	public GUIText countText;
+	private int count;
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count : " + count.ToString();
+	}
+
+}
